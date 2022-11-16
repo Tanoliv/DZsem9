23868fb0211af93a96dde23925a0ad8de7705056
Oleg <tmr777@mail.ru>
2022-11-16 10:38:02 +0300

    Задача 64

---
+++ Sem9DzZd64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число:\t");
+int N =int.Parse(Console.ReadLine()!);
+
+PrintNum( N, 1);
+
+void PrintNum(int n, int m)
+{
+    if (m > n) return ;   
+    
+    else
+    {
+        PrintNum(n, m + 1);
+        Console.Write($"{m + " "}| ");
+    }
+}   
